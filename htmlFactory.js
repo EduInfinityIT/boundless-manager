04f628bc8093e9b7e1d5e096247fd9c9021c4c55
Chriest Yu <jcppman@gmail.com>
2014-07-21 05:30:20 +0800

    Almost done

--- htmlFactory.js
+++ htmlFactory.js
@@ -1,21 +1,14 @@
 var fs = require('fs');
 var path = require('path');
 var jade = require('jade');
-var comment = process.argv[2];
-var filenames = process.argv.slice(3);
 
-function buildCompiler (filename) {
+var template = fs.readFileSync('./template/lecture.jade');
+var compiler = jade.compile(template);
 
-  var template = fs.readFileSync(filename);
-  var compiler = jade.compile(template);
+exports.generateHTML = function (comment, filename) {
 
-  return compiler;
-
-}
-
-var compiler = buildCompiler('./template/lecture.jade');
-
-var getHTML = function (comment, filename) {
+  var ext = path.extname(filename);
+  var newFilename = filename.replace(ext, '.htm');
 
   var file = fs.readFileSync(filename);
   var content = file.toString().split('\n');
@@ -27,15 +20,3 @@ var getHTML = function (comment, filename) {
   return result;
 
 };
-
-var generateHTML = function (comment, filename) {
-
-  var ext = path.extname(filename);
-  var newFilename = filename.replace(ext, '.htm');
-
-  console.log('Generate HTML from ' + filename + ' to ' + newFilename);
-  fs.writeFileSync(newFilename, getHTML(comment, filename));
-
-};
-
-exports.generateHTML = generateHTML;
--- index.js
+++ index.js
@@ -1,137 +1,214 @@
 var commander = require('commander');
+var htmlFactory = require('./htmlFactory');
 
 commander.version('0.0.1')
   .option('-c --comment [comment Msg]', 'Add comment')
   .option('-f --folder [folderId]', 'Get links form GDrive folder')
-  .option('-g --generate', 'Generate HTML files')
   .parse(process.argv);
 
-var generate = commander.generate;
+var comment = commander.comment;
+var filenames = commander.args;
 
-if (generate) {
+var folderId = commander.folder;
+var fs = require('fs');
+var async = require('async');
+var readline = require('readline');
+var credFile = './cred.json';
+var credential = require(credFile);
+var googleapis = require('googleapis');
 
-  var comment = commander.comment;
-  var filenames = commander.args;
-  var htmlFactory = require('./htmlFactory');
+var CLIENT_ID = credential.installed.client_id;
+var CLIENT_SECRET = credential.installed.client_secret;
+var REDIRECT_URL = credential.installed.redirect_uris[0];
+var SCOPE = 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file';
 
-  for (var i in filenames) {
+var auth = new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
-    var filename = filenames[i];
-    htmlFactory.generateHTML(comment, filename);
-  
-  }
+var pipeline = function (comment, filename, folderId, next) {
 
-} else {
-
-  var folderId = commander.folder;
-  var fs = require('fs');
-  var async = require('async');
-  var readline = require('readline');
-  var credFile = './cred.json';
-  var credential = require(credFile);
-  var googleapis = require('googleapis');
-
-  var CLIENT_ID = credential.installed.client_id;
-  var CLIENT_SECRET = credential.installed.client_secret;
-  var REDIRECT_URL = credential.installed.redirect_uris[0];
-  var SCOPE = 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file';
-
-  var auth = new googleapis.OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+  var res = filename.match(/([^\/\\]+)\.\w*/);
+  if (!res) {
+    next('Invalid filename'); 
+    return;
+  } 
+  var basename = res[1];
+  var html = htmlFactory.generateHTML(comment, filename);
 
   async.waterfall([
-
-    // Ask for Code
-    function (cb) {
-
-      if (credential.tokens) {
-
-        cb(null, credential.tokens);
-        return;
-      
-      }
-
-      var url = auth.generateAuthUrl({
-        access_type: 'offline', // will return a refresh token
-        scope: SCOPE
-      });
-
-      var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-
-      rl.question('Please Visit: ' + url, function (code) {
-
-        auth.getToken(code, function(err, tokens) {
-          
-          cb(err, tokens);
-
-        });
-      
-      });
-
-    },
-
-    // Update the tokens
-    function (tokens, cb) {
-
-      auth.setCredentials(tokens);
-      credential.tokens = tokens;
-      fs.writeFile(credFile, JSON.stringify(credential), cb);
-    
-    },
 
     // Get client
     function (cb) {
 
       googleapis.discover('drive', 'v2').execute(cb);
-  
+
     },
 
-    // List All Documents
+    // Upload
     function (client, cb) {
+      
+      console.log('Uploading ' + filename);
+      client.drive.files.insert({
+        title: basename+'.html',
+        mimeType: 'text/html',
+        convert: true 
+      })
+      .withMedia('text/html', html)
+      .withAuthClient(auth)
+      .execute(function (err, fileResponse) {
 
-      client.drive.children
-        .list({
-          folderId: folderId,
-          q: "mimeType contains 'document'"
-        })
-        .withAuthClient(auth)
-        .execute(function (err, listResult) {
+        if (err) {
+          cb(err);
+          return; 
+        }
 
-          if (err) {
-            cb(err); 
-            return;
-          }
-
-          var items = listResult.items;
-          var ids = items.map(function (item) {
-            return item.id; 
-          });
-
-          cb(null, client, ids);
-
-        });
-
+        cb(null, client, fileResponse);
+      
+      });
+    
     },
 
-    // Get alternative links
-    function (client, ids, cb) {
+    // Rename & Move to new folder
+    function (client, fileResponse, cb) {
 
-      //TODO
-      console.log(ids);
-      cb();
+      var fileId = fileResponse.id;
+      var originalFolderId = fileResponse.parents[0].id;
+      var ops = {
+        fileId: fileId 
+      };
+
+      console.log('Rename file as ' + basename);
+
+      if (folderId) {
+
+        console.log('Move file from ' + originalFolderId + ' to folder ' + folderId);
+        ops.addParents = folderId;
+        ops.removeParents = originalFolderId;
+      
+      }
+
+      client.drive.files.patch(ops, {
+        title: basename 
+      }).withAuthClient(auth)
+      .execute(function (err, fileResponse) {
+
+        cb(err, fileResponse);
+      
+      });
+      
+    },
+
+    // Get link
+    function (fileResponse, cb) {
+
+      var link = fileResponse.alternateLink;
+      cb(null, link);
     
     }
-     
-  ], function (err) {
+  
+  ], function (err, link) {
 
-    if (err) {
-
-      console.log('Error:'+JSON.stringify(err));
-    
-    }
-
+    var realLink = link.replace(/\?usp.*/, '');
+    next(null, {
+      title: basename,
+      link: realLink 
+    });
+  
   });
 
-}
+};
+
+async.waterfall([
+
+  // Ask for Code
+  function (cb) {
+
+    if (credential.tokens) {
+
+      cb(null, credential.tokens);
+      return;
+    
+    }
+
+    var url = auth.generateAuthUrl({
+      access_type: 'offline', // will return a refresh token
+      scope: SCOPE
+    });
+
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question('Please Visit: ' + url, function (code) {
+
+      auth.getToken(code, function(err, tokens) {
+        
+        cb(err, tokens);
+
+      });
+    
+    });
+
+  },
+
+  // Update the tokens
+  function (tokens, cb) {
+
+    auth.setCredentials(tokens);
+    credential.tokens = tokens;
+    fs.writeFile(credFile, JSON.stringify(credential), cb);
+  
+  },
+
+  // For everyone
+  function (cb) {
+
+    var results = [];
+    async.eachSeries(filenames, function (filename, next) {
+
+      pipeline(comment, filename, folderId, function (err, result) {
+
+        if (err) {
+          next(err);
+          return; 
+        }
+
+        results.push(result);
+        next();
+      
+      });
+    
+    }, function (err) {
+
+      if (err) {
+        cb(err); 
+        return;
+      }    
+
+      cb(null, results);
+
+    });
+  
+  },
+
+], function (err, results) {
+
+  if (err) {
+
+    console.log('Error:'+JSON.stringify(err));
+  
+  } else {
+
+    var output = results.map(function (item) {
+      var link = item.link;
+      var title = item.title; 
+
+      return (title + ',' + link);
+
+    });
+
+    console.log(output.join('\n'));
+  
+  }
+
+});
